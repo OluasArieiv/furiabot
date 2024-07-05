022c13ebe617dc411845b35508075a0165dd1d68
OluasArieiv <130432135+OluasArieiv@users.noreply.github.com>
2024-07-05 01:02:08 -0300

    altars.sh

--- altars.sh
+++ altars.sh
@@ -5,7 +5,7 @@ altars_fight () {
  apply_event
 
  #/enterFight
- local LA=6 # interval attack
+ local LA=12 # interval attack
  echo "30" >HPER # % to heal
  echo "15" >RPER # % to random
  cf_access () {
