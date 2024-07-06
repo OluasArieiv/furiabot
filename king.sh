b8ded8c1fd4ff416b6ad42af39d56f6bd976ba2c
OluasArieiv <130432135+OluasArieiv@users.noreply.github.com>
2024-07-06 14:12:12 -0300

    king.sh

--- king.sh
+++ king.sh
@@ -7,7 +7,7 @@ king_fight () {
 
  #/enterFight
  cd $TMP
- local LA=16 # interval attack
+ local LA=100 # interval attack
  local HPER="38" # % to heal
  local RPER=5 # % to random
  cl_access () {
