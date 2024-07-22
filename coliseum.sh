478b47365316d3df70e8a8b79db1286f0c82aeeb
OluasArieiv <130432135+OluasArieiv@users.noreply.github.com>
2024-07-22 11:28:36 -0300

    coliseum.sh

--- coliseum.sh
+++ coliseum.sh
@@ -15,7 +15,7 @@ coliseum_fight () {
  ) &
  time_exit 20
  #/enterFight
- local LA=4500    #2.Interval attack
+ local LA=6750    #2.Interval attack
  local HPER=30 #3.HPER % to heal
  local RPER=5 #4.RPER % to random
  printf "\nColiseum ...\n"
